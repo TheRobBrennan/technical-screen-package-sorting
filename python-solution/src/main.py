51699b61c22c1db3a6564ce0793208021549c30d
Rob Brennan <rob@therobbrennan.com>
2024-02-20 22:16:27 -0800

    Refactor package details into a dictionary

--- python-solution/src/main.py
+++ python-solution/src/main.py
@@ -1,13 +1,19 @@
 # src/main.py
 from src.lib.sort import sort_package
 
-# Example usage
 if __name__ == "__main__":
-    # Example package
-    width = 100
-    height = 100
-    length = 100
-    mass = 10
-    stack = sort_package(width, height, length, mass)
-    print(f"\n----------------\nPackage details\n----------------\nWidth: {width} cm, Height: {height} cm, Length: {length} cm, Mass: {mass} kg")
+    # Refactor package details into a dictionary
+    package_details = {
+        "width": 100,
+        "height": 100,
+        "length": 100,
+        "mass": 10,
+    }
+    
+    # Unpack the dictionary when calling the sort_package function
+    stack = sort_package(**package_details)
+    
+    # Display the package details and the stack it belongs to
+    package_description = ', '.join(f"{key}: {value}" for key, value in package_details.items())
+    print(f"Package details =>\t{package_description}")
     print(f"This package belongs to the {stack} stack.\n")
