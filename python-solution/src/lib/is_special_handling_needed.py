663eab09c5aae2d45610689c47e7c8920bbc3b98
Rob Brennan <rob@therobbrennan.com>
2024-02-20 22:03:05 -0800

    Refactor functionality to modularize checks to see if the package is heavy and/or bulky

--- python-solution/src/lib/is_special_handling_needed.py
+++ python-solution/src/lib/is_special_handling_needed.py
@@ -1,14 +1,7 @@
 # src/lib/is_special_handling_needed.py
-from ..constants import STANDARD_PACKAGE
+from .is_heavy import is_heavy
+from .is_bulky import is_bulky
 
 def is_special_handling_needed(width: float, height: float, length: float, mass: float) -> bool:
     """Determine if special handling is needed based on package dimensions and mass."""
-    volume = width * height * length
-    is_heavy = mass >= STANDARD_PACKAGE["MAX_MASS_IN_KG"]
-    is_bulky = (
-        width >= STANDARD_PACKAGE["MAX_WIDTH_IN_CM"] or
-        height >= STANDARD_PACKAGE["MAX_HEIGHT_IN_CM"] or
-        length >= STANDARD_PACKAGE["MAX_LENGTH_IN_CM"] or
-        volume >= STANDARD_PACKAGE["MAX_VOLUME_IN_CUBIC_CM"]
-    )
-    return is_heavy or is_bulky
+    return is_heavy(mass) or is_bulky(width, height, length)
