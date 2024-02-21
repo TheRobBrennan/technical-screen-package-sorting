889fb0b2419fc42e8297b281fd945347b31283e3
Rob Brennan <rob@therobbrennan.com>
2024-02-20 17:11:26 -0800

    Add ability to run tests and generate coverage against our Python code

--- recipes/nextjs-fastapi/api/index.py
+++ recipes/nextjs-fastapi/api/index.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from datetime import datetime
 
-app = FastAPI()
+def get_current_timestamp():
+    # Format the timestamp as you like, here it's in ISO format - "2024-02-20T15:23:53.783919"
+    return datetime.now().isoformat()
 
+app = FastAPI()
 @app.get("/api/ping")
 def ping():
-    current_time = datetime.now()
-    # Format the timestamp as you like, here it's in ISO format - "2024-02-20T15:23:53.783919"
-    timestamp = current_time.isoformat()
+    timestamp = get_current_timestamp()
     return {"message": "Pong.", "timestamp": timestamp}
