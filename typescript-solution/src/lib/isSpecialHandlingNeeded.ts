566986402df09eabef2b7be130e64c005e56ed8a
Rob Brennan <rob@therobbrennan.com>
2024-02-20 21:02:19 -0800

    Improve comments for isSpecialHandlingNeeded

--- typescript-solution/src/lib/isSpecialHandlingNeeded.ts
+++ typescript-solution/src/lib/isSpecialHandlingNeeded.ts
@@ -9,7 +9,13 @@ import { isPackageHeavy } from "./isPackageHeavy";
  * @param height The height of the package in cm.
  * @param length The length of the package in cm.
  * @param mass The mass of the package in kg.
- * @returns Returns true if the package meets or exceeds the criteria for being considered heavy or bulky, indicating that it requires special handling. Specifically, a package is deemed to require special handling if its mass is equal to or greater than the maximum mass threshold, or if any of its dimensions are equal to or larger than the maximum dimension thresholds, thus making it bulky. Returns false if the package does not meet these criteria and thus does not require special handling.
+ * @returns Returns true if the package meets or exceeds the criteria for being considered heavy or bulky, indicating that it requires special handling.
+ * 
+ *          Specifically, a package is deemed to require special handling if: 
+ *            - Its mass is equal to or greater than the maximum mass threshold 
+ *            - Any of its dimensions are equal to or larger than the maximum dimension thresholds, thus making it bulky.
+ * 
+ *          Returns false if the package does not meet these criteria and thus does not require special handling.
  */
 export function isSpecialHandlingNeeded(width: number, height: number, length: number, mass: number): boolean {
   const isHeavy = isPackageHeavy(mass);
