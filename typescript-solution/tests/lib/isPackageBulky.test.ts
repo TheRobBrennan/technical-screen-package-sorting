e41be354993dc0a46753dab97eeab7653cfea20b
Rob Brennan <rob@therobbrennan.com>
2024-02-20 21:06:03 -0800

    Add (width, height, or length) to dimension comments

--- typescript-solution/tests/lib/isPackageBulky.test.ts
+++ typescript-solution/tests/lib/isPackageBulky.test.ts
@@ -7,7 +7,7 @@ describe('isPackageBulky', () => {
     expect(isPackageBulky(99.9, 99.9, 99.9)).toBe(false);
   });
 
-  test('returns false for packages with the longest dimension just shy of the max dimension', () => {
+  test('returns false for packages with the longest dimension (width, height, or length) just shy of the max dimension', () => {
     const almostMaxWidth = STANDARD_PACKAGE.MAX_WIDTH_IN_CM - 0.1;
     const almostMaxHeight = STANDARD_PACKAGE.MAX_HEIGHT_IN_CM - 0.1;
     const almostMaxLength = STANDARD_PACKAGE.MAX_LENGTH_IN_CM - 0.1;
@@ -22,13 +22,13 @@ describe('isPackageBulky', () => {
     expect(isPackageBulky(100, 100, bulkyVolume / 10_000)).toBe(true);
   });
 
-  test('returns true for packages with one dimension equal to the max dimension', () => {
+  test('returns true for packages with one dimension (width, height, or length) equal to the max dimension', () => {
     expect(isPackageBulky(STANDARD_PACKAGE.MAX_WIDTH_IN_CM, 100, 100)).toBe(true);
     expect(isPackageBulky(100, STANDARD_PACKAGE.MAX_HEIGHT_IN_CM, 100)).toBe(true);
     expect(isPackageBulky(100, 100, STANDARD_PACKAGE.MAX_LENGTH_IN_CM)).toBe(true);
   });
 
-  test('returns true for packages with one dimension greater than the max dimension', () => {
+  test('returns true for packages with one dimension (width, height, or length) greater than the max dimension', () => {
     const bulkyWidth = STANDARD_PACKAGE.MAX_WIDTH_IN_CM + 10;
     const bulkyHeight = STANDARD_PACKAGE.MAX_HEIGHT_IN_CM + 10;
     const bulkyLength = STANDARD_PACKAGE.MAX_LENGTH_IN_CM + 10;
--- typescript-solution/tests/lib/isPackageReal.test.ts
+++ typescript-solution/tests/lib/isPackageReal.test.ts
@@ -2,17 +2,17 @@
 import { isPackageReal } from '../../src/lib/isPackageReal';
 
 describe('isPackageReal', () => {
-  test('returns true for positive dimensions and mass', () => {
+  test('returns true for positive dimensions (width, height, or length) and mass', () => {
     expect(isPackageReal(10, 20, 30, 5)).toBe(true);
   });
 
-  test('returns false if any dimension is zero', () => {
+  test('returns false if any dimension (width, height, or length) is zero', () => {
     expect(isPackageReal(0, 20, 30, 5)).toBe(false);
     expect(isPackageReal(10, 0, 30, 5)).toBe(false);
     expect(isPackageReal(10, 20, 0, 5)).toBe(false);
   });
 
-  test('returns false if any dimension is negative', () => {
+  test('returns false if any dimension (width, height, or length) is negative', () => {
     expect(isPackageReal(-1, 20, 30, 5)).toBe(false);
     expect(isPackageReal(10, -1, 30, 5)).toBe(false);
     expect(isPackageReal(10, 20, -1, 5)).toBe(false);
