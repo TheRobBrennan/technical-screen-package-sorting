2d74941f470f18878657518cf88e50c022c78d6d
Rob Brennan <rob@therobbrennan.com>
2024-02-20 22:19:16 -0800

    Define a collection of package details in a list of dictionaries

--- python-solution/src/main.py
+++ python-solution/src/main.py
@@ -1,19 +1,21 @@
 # src/main.py
 from src.lib.sort import sort_package
 
+# Example usage
 if __name__ == "__main__":
-    # Refactor package details into a dictionary
-    package_details = {
-        "width": 100,
-        "height": 100,
-        "length": 100,
-        "mass": 10,
-    }
+    # Define a collection of package details in a list of dictionaries
+    packages = [
+        {"width": 100, "height": 100, "length": 100, "mass": 10},
+        {"width": 150, "height": 200, "length": 250, "mass": 22},  # Example of a heavy package
+        {"width": 160, "height": 100, "length": 100, "mass": 10},  # Example of a bulky package
+        {"width": 0, "height": 100, "length": 100, "mass": 10},    # Example of an invalid package
+    ]
     
-    # Unpack the dictionary when calling the sort_package function
-    stack = sort_package(**package_details)
-    
-    # Display the package details and the stack it belongs to
-    package_description = ', '.join(f"{key}: {value}" for key, value in package_details.items())
-    print(f"Package details =>\t{package_description}")
-    print(f"This package belongs to the {stack} stack.\n")
+    # Process each package in the collection
+    for package in packages:
+        stack = sort_package(**package)
+        
+        # Display the package details and the stack it belongs to
+        package_description = ', '.join(f"{key}: {value}" for key, value in package.items())
+        print(f"\nPackage details:\n\t{package_description}")
+        print(f"\tThis package belongs to the {stack} stack.\n")
